dd7a420abc3a50581c6c021efa45a8a9a710255a
Tony Jung <tonyjung@Tonys-MacBook-Air.local>
2022-11-06 22:04:56 -0500

    Added error handling

--- hw3b.php
+++ hw3b.php
@@ -7,7 +7,7 @@
 $dbhost = 'dbase.cs.jhu.edu';
 $dbuser = 'YOUR_SQL_USERNAME';
 $dbpass = 'YOUR_SQL_PASSWORD';
-$dbname = 'YOUR_SQL_DATABASE';
+$dbname = 'YOUR_DATABASE';
 $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s<br>", mysqli_connect_error());
@@ -17,16 +17,20 @@ $ssn = $_GET["ssn"];
 if ($mysqli->multi_query("CALL ShowRawScores('".$ssn."');")) {
     if ($result = $mysqli->store_result()) {
         $row = $result->fetch_row();
-        printf("HW1: %s<br>", $row[0]);
-        printf("HW2a: %s<br>", $row[1]);
-        printf("HW2b: %s<br>", $row[2]);
-        printf("Midterm: %s<br>", $row[3]);
-        printf("HW3 %s<br>", $row[4]);
-        printf("FExam: %s<br>", $row[3]);
-        $mysqli->next_result();
-        $result = $mysqli->store_result();
-        $row = $result->fetch_row();
-        printf("%s<br>", $row[0]);
+        if (count($row) == 1) {
+            printf("%s", $row[0]);
+        } else {
+            printf("HW1: %s<br>", $row[0]);
+            printf("HW2a: %s<br>", $row[1]);
+            printf("HW2b: %s<br>", $row[2]);
+            printf("Midterm: %s<br>", $row[3]);
+            printf("HW3 %s<br>", $row[4]);
+            printf("FExam: %s<br>", $row[3]);
+            $mysqli->next_result();
+            $result = $mysqli->store_result();
+            $row = $result->fetch_row();
+            printf("%s<br>", $row[0]);
+        }
         $result->close();
     }
 } else {
