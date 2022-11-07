7d49bf238643ee8e783f41e689ef6ced4b7f0606
Tony Jung <tonyjung@Tonys-MacBook-Air.local>
2022-11-06 21:46:30 -0500

    Updated 3d php

--- hw3d.php
+++ hw3d.php
@@ -5,9 +5,9 @@
 
 <?php
 $dbhost = 'dbase.cs.jhu.edu';
-$dbuser = 'YOUR_SQL_USERNAME';
-$dbpass = 'YOUR_SQL_PASSWORD';
-$dbname = 'YOUR_SQL_DATABASE';
+$dbuser = '22fa_tjung8';
+$dbpass = 'OiJPmTsrwr';
+$dbname = '22fa_tjung8_db';
 $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s<br>", mysqli_connect_error());
@@ -15,20 +15,19 @@ if (mysqli_connect_errno()) {
 }
 $pword = $_GET["pword"];
 if ($mysqli->multi_query("CALL AllPercentages('".$pword."');")) {
-    printf("LName | FName | Section | HW1 | HW2a | HW2b | Midterm | HW3 | FExam | Weighted Average<br>");
-    printf("---------------------------------------------------------------------------------------------------------------------<br>");
-    do {
-        if ($result = $mysqli->store_result()) {
-            while ($row = $result->fetch_row()) {
-                if (count($row) == 1) {
-                    printf("%s", $row[0]);
-                } else {
-                    printf("%s | %s | %s | %s | %s | %s | %s | %s | %s | %s<br>", $row[0], $row[1], $row[2], $row[3], $row[4], $row[5], $row[6], $row[7], $row[8], $row[9]);
-                }
-            }
-            $result->close();
+    if ($result = $mysqli->store_result()) {
+        $row = $result->fetch_row();
+        if (count($row) == 1) {
+            printf("%s", $row[0]);
+        } else {
+            printf("LName | FName | Section | HW1 | HW2a | HW2b | Midterm | HW3 | FExam | Weighted Average<br>");
+            printf("---------------------------------------------------------------------------------------------------------------------<br>");
+            do {
+                printf("%s | %s | %s | %s | %s | %s | %s | %s | %s | %s<br>", $row[0], $row[1], $row[2], $row[3], $row[4], $row[5], $row[6], $row[7], $row[8], $row[9]);
+            } while ($row = $result->fetch_row());
         }
-    } while ($mysqli->next_result());
+        $result->close();
+    }
 } else {
     printf("<br>Error: %s\n", $mysqli->error);
 }
