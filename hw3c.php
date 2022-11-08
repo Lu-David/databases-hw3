5970381061151eb6f9fbb8600a0355f653242f56
David Lu <45726258+Lu-David@users.noreply.github.com>
2022-11-08 11:14:31 -0500

    Update hw3c.php

--- hw3c.php
+++ hw3c.php
@@ -23,7 +23,7 @@ if ($mysqli->multi_query("CALL AllRawScores('".$pword."');")) {
             printf("LName | FName | Section | HW1 | HW2a | HW2b | Midterm | HW3 | FExam <br>");
             printf("---------------------------------------------------------------------------------------------------------------------<br>");
             do {
-                printf("%s | %s | %s | %s | %s | %s | %s | %s | %s | %s<br>", $row[0], $row[1], $row[2], $row[3], $row[4], $row[5], $row[6], $row[7], $row[8]);
+                printf("%s | %s | %s | %s | %s | %s | %s | %s | %s <br>", $row[0], $row[1], $row[2], $row[3], $row[4], $row[5], $row[6], $row[7], $row[8]);
             } while ($row = $result->fetch_row());
         }
         $result->close();
