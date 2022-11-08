8e8697e8805d05bde61b4adf40581393959cc743
David Lu <45726258+Lu-David@users.noreply.github.com>
2022-11-07 21:34:07 -0500

    added starter for problem c

--- procedures.sql
+++ procedures.sql
@@ -11,6 +11,32 @@ CREATE VIEW Weights AS
 CREATE VIEW WtdPts AS
     SELECT (1/Totpts.HW1)*Weight.HW1 HW1, (1/Totpts.HW2a)*Weight.HW2a HW2a, (1/Totpts.HW2b)*Weight.HW2b HW2b, (1/Totpts.Midterm)*Weight.Midterm Midterm, (1/Totpts.HW3)*Weight.HW3 HW3, (1/Totpts.FExam)*Weight.FExam FExam
     FROM Totpts, Weight;
+    
+-- a) Print a single student’s raw scores:
+DROP PROCEDURE IF EXISTS ShowRawScores;
+DELIMITER |
+CREATE PROCEDURE ShowRawScores (IN SSN VARCHAR(4))
+    BEGIN
+        DECLARE rowSSN VARCHAR(4);
+
+        DECLARE get_student CURSOR FOR
+            SELECT Rawscores.SSN
+            FROM Rawscores
+            WHERE Rawscores.SSN=SSN;
+
+        DECLARE EXIT HANDLER FOR NOT FOUND
+        SELECT 'STUDENT NOT FOUND' AS 'Error Message';
+
+        OPEN get_student;
+        FETCH get_student INTO rowSSN;
+        CLOSE get_student;
+
+        SELECT *
+        FROM Rawscores
+        WHERE Rawscores.SSN=SSN;
+    END;
+|
+DELIMITER ;
 
 --(b) Print a single student’s percentage scores and weighted average:
 DROP PROCEDURE IF EXISTS ShowPercentages;
@@ -38,6 +64,32 @@ CREATE PROCEDURE ShowPercentages (IN SSN VARCHAR(4))
         SELECT CONCAT('The cumulative course average for ', Rawscores.FName, ' ', Rawscores.LName, ' ',  'is ', (Rawscores.HW1*WtdPts.HW1+Rawscores.HW2a*WtdPts.HW2a+Rawscores.HW2b*WtdPts.HW2b+Rawscores.Midterm*WtdPts.Midterm+Rawscores.HW3*WtdPts.HW3+Rawscores.FExam*WtdPts.FExam)) Weighted_average
         FROM Rawscores, WtdPts
         WHERE Rawscores.SSN=SSN;
+    END;
+|
+DELIMITER ;
+
+--(c) Print a full table of the raw class scores
+DROP PROCEDURE IF EXISTS AllRawScores;
+DELIMITER |
+CREATE PROCEDURE AllRawScores(IN password VARCHAR(4000))
+    BEGIN
+        DECLARE rowPassword VARCHAR(4000);
+
+        DECLARE get_password CURSOR FOR
+            SELECT CurPasswords
+            FROM Passwords
+            WHERE CurPasswords=password;
+
+        DECLARE EXIT HANDLER FOR NOT FOUND
+        SELECT 'INVALID PASSWORD' AS 'Error Message';
+
+        OPEN get_password;
+        FETCH get_password INTO rowPassword;
+        CLOSE get_password;
+
+        SELECT *
+        FROM Rawscores
+        ORDER BY Rawscores.Section;
     END;
 |
 DELIMITER ;
