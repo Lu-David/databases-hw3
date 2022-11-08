6e03937bdee284290df9b9403ce0620e467dd4fc
David Lu <45726258+Lu-David@users.noreply.github.com>
2022-11-08 15:13:53 -0500

    Update hw3b.php

--- hw3b.php
+++ hw3b.php
@@ -20,16 +20,17 @@ if ($mysqli->multi_query("CALL ShowRawScores('".$ssn."');")) {
         if (count($row) == 1) {
             printf("%s", $row[0]);
         } else {
-            printf("HW1: %s<br>", $row[0]);
-            printf("HW2a: %s<br>", $row[1]);
-            printf("HW2b: %s<br>", $row[2]);
-            printf("Midterm: %s<br>", $row[3]);
-            printf("HW3 %s<br>", $row[4]);
-            printf("FExam: %s<br>", $row[3]);
+            printf("Name: %s %s<br>", $row[1], $row[2]);
+            printf("Section: %s<br>", $row[3]);
+            printf("HW1: %s<br>", $row[4]);
+            printf("HW2a: %s<br>", $row[5]);
+            printf("HW2b: %s<br>", $row[6]);
+            printf("Midterm: %s<br>", $row[7]);
+            printf("HW3 %s<br>", $row[8]);
+            printf("FExam: %s<br>", $row[9]);
             $mysqli->next_result();
             $result = $mysqli->store_result();
             $row = $result->fetch_row();
-            printf("%s<br>", $row[0]);
         }
         $result->close();
     }
